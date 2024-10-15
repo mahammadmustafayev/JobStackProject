58949800dbafd56b74827d53010d366777e87fa9
mahammadmustafayev <mustafamehmed1251@gmail.com>
2024-10-15 10:22:23 +0400

    added

--- JobStack/src/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ JobStack/src/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8b489c25dfcf6584d8da0057e8d4ecce208541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58abb152a20de0ce309bd56cbf36891c8f81058")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
